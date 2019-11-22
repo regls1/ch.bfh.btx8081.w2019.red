14901053a5e45444940f46bfb649972492a0ceb4
Stefanie Neuenschwander <48947407+neues4@users.noreply.github.com>
2019-11-22 16:01:06 +0100

    contact sample view

--- src/main/java/view/ContactView.java
+++ src/main/java/view/ContactView.java
@@ -1,5 +1,13 @@
 package view;
 
-public class ContactView {
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
 
+@Route
+public class ContactView extends VerticalLayout {
+
+	public ContactView() {
+		add(new H1("I am a sample contact view"));
+	}
 }
