c17a84ac8e72e520da9e2bfa11f08d2e5997f152
Stefanie Neuenschwander <48947407+neues4@users.noreply.github.com>
2019-11-22 14:26:54 +0100

    Exercise View Sample

--- src/main/java/view/ExerciseView.java
+++ src/main/java/view/ExerciseView.java
@@ -1,5 +1,16 @@
 package view;
 
-public class ExerciseView {
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
 
+@Route
+public class ExerciseView extends VerticalLayout {
+
+	public ExerciseView() {
+		add(new H1("I am a sample view"));
+	}
 }
+
+
+
