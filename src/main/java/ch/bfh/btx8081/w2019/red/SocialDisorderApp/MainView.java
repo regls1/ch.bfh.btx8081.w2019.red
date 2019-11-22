ed123222b07c63348b1bd6f3207ac1282e010401
Selina Regli <56729195+regls1@users.noreply.github.com>
2019-11-22 15:37:07 +0100

    updated mainview

--- src/main/java/ch/bfh/btx8081/w2019/red/SocialDisorderApp/MainView.java
+++ src/main/java/ch/bfh/btx8081/w2019/red/SocialDisorderApp/MainView.java
@@ -1,11 +1,13 @@
 package ch.bfh.btx8081.w2019.red.SocialDisorderApp;
 import view.ExerciseView;
+import view.DiaryView;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.upload.UploadI18N.Units;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
@@ -21,18 +23,19 @@ public class MainView extends VerticalLayout {
      	HorizontalLayout l = new HorizontalLayout();
      	Button contact = new Button("Contact", event -> Notification.show("Clicked!"));
         add(contact);
+        //add(new RouterLink("Go to the sample contact view!", ContactView.class));
         Button exercise = new Button("Exercise", event -> Notification.show("Clicked!"));
         add(exercise);
+        add(new RouterLink("Go to the sample exercise view!", ExerciseView.class));
         Button diary = new Button("Diary", event -> Notification.show("Clicked!"));
         add(diary);
+        //add(new RouterLink("Go to the sample diary view!", DiaryView.class));
         l.add(contact, exercise, diary);
         add(l);
         
         Button button = new Button(new Image("img/contact.png", "Contact logo"));
         add(button);
-        
-        Button test = new Button(add(new RouterLink("Exercise", ExerciseView.class)));
-        add(test);
+  
         
   
     }
