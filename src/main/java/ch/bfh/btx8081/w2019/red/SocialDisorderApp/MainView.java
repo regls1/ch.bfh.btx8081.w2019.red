fdfc4e06c2105c7b1b8e17eb3df28976327da620
Selina Regli <56729195+regls1@users.noreply.github.com>
2019-11-24 21:44:57 +0100

    updated mainview

--- src/main/java/ch/bfh/btx8081/w2019/red/SocialDisorderApp/MainView.java
+++ src/main/java/ch/bfh/btx8081/w2019/red/SocialDisorderApp/MainView.java
@@ -6,6 +6,8 @@ import view.ContactView;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -22,30 +24,33 @@ public class MainView extends VerticalLayout {
 
     public MainView() {
      	HorizontalLayout l = new HorizontalLayout();
-     	Button contact = new Button("Diary", event -> Notification.show("Clicked!"));
-        add(contact);
-        add(new RouterLink("Go to the sample contact view!", ContactView.class));
-        Button exercise = new Button("Exercise", event -> Notification.show("Clicked!"));
+     	//Button contact = new Button("Contact", event -> Notification.show("Clicked!"));
+     	
+     	Button contact = new Button("Contact", new Icon(VaadinIcon.USER_CARD));
+     	contact.setIconAfterText(true);
+        contact.setHeight("550px");
+        contact.setWidth("350px");
+     	add(contact);
+        contact.addClickListener(e -> UI.getCurrent().navigate(ContactView.class));
+     
+        Button exercise = new Button("Exercise", new Icon(VaadinIcon.CHECK_SQUARE_O));
+        exercise.setIconAfterText(true);
+        exercise.setHeight("550px");
+        exercise.setWidth("350px");
         add(exercise);
-        add(new RouterLink("Go to the sample exercise view!", ExerciseView.class));
-        Button diary = new Button("Diary", event -> Notification.show("Clicked!"));
+        exercise.addClickListener(e -> UI.getCurrent().navigate(ExerciseView.class));
+       
+        Button diary = new Button("Diary", new Icon(VaadinIcon.CALENDAR));
+        diary.setIconAfterText(true);
+        diary.setHeight("550px");
+        diary.setWidth("350px");
         add(diary);
-        add(new RouterLink("Go to the sample diary view!", DiaryView.class)); 
-        
-        
+        diary.addClickListener(e -> UI.getCurrent().navigate(DiaryView.class));
+     
         l.add(contact, exercise, diary);
         add(l);
         
-        /**
-        Button contact1 = new Button(new Image("img/contact.png", "Contact logo"));
-        add(contact1);
-        
-        Button calender = new Button(new Image("img/calender.png", "Calender logo"));
-        add(calender);
-        */
-          
-  
+        //Button contact1 = new Button(new Image("img/contact.png", "Contact logo"));
+        //add(contact1);
     }
-    
-    
 }
