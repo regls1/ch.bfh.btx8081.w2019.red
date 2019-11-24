a03d0a6262c75be4207ac4a6b3b04b48a5ce076b
Stefanie Neuenschwander <48947407+neues4@users.noreply.github.com>
2019-11-24 17:59:43 +0100

    added comment to exercise view

--- src/main/java/view/ExerciseView.java
+++ src/main/java/view/ExerciseView.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+// WIRD BEARBEITET DURCH NEUES4!
+
 @Route
 public class ExerciseView extends VerticalLayout {
 
