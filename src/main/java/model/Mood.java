becbefb5603925cc66dbd7f9d4cbd296e90fb9b1
ToWack <49730637+ToWack@users.noreply.github.com>
2019-12-06 12:56:08 +0100

    modified mood entity class for persistence

--- src/main/java/model/Mood.java
+++ src/main/java/model/Mood.java
@@ -1,24 +1,29 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * model class of mood
  * 
  * @author wackt2
  *
  */
+@Entity
 public class Mood {
 	// primary key
-	int moodId;
-	String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private int moodId;
+	private String name;
 	
 	/*
 	 * getter and setter methods
 	 */
 	public int getMoodId() {
 		return moodId;
-	}
-	public void setMoodId(int moodId) {
-		this.moodId = moodId;
 	}
 	public String getName() {
 		return name;
