ab16d0bed3f20be13e7106ba861d80f3e77a00a9
romap1 <56729339+romap1@users.noreply.github.com>
2019-12-09 11:03:44 +0100

    new methodes in entityService

--- src/main/java/model/EntityService.java
+++ src/main/java/model/EntityService.java
@@ -2,7 +2,9 @@ package model;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class EntityService {
 
@@ -27,14 +29,37 @@ public class EntityService {
 	
 	public static EntityManager getManager() {
 		return em;
+
 	}
 	
 	
+    public void setUp() {
+        // Begin a new local transaction so that we can persist a new entity
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+	}
+	
+
+    public void close() {
+        // It is always good practice to close the EntityManager so that resources
+        // are conserved.
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+//	Query q = em.createNativeQuery("SHUTDOWN COMPACT");
+//	q.executeUpdate();
+        em.flush();
+        transaction.commit();
+        em.close();
+    }
+	
 	public static void main(String[] args) {
 		EntityService es = new EntityService();
-		EntityManager em = es.getManager();
-		em.getTransaction();
-		em.close();
+		//EntityManager em = es.getManager();
+		es.setUp();
+		es.close();
+		//em.getTransaction();
+		//em.close();
 	}
 	
 }
