773b1213b885fd8c7a15969f788bf4e9394f4e93
romap1 <56729339+romap1@users.noreply.github.com>
2019-12-12 21:58:50 +0100

    documenting EntityService class

--- src/main/java/model/EntityService.java
+++ src/main/java/model/EntityService.java
@@ -5,78 +5,76 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 /**
+ * EntityService creates only one EntityManager by using the Singleton pattern.
  * 
  * @author Patricia
  *
  */
 public class EntityService {
-	
-	  private static final String PERSISTENCE_UNIT_NAME = "ch.bfh.btx8081.w2019.red.git";
-	  protected static EntityManager em = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME).createEntityManager();
-	  private static EntityService instance;
 
-	 /**
-	  * 
-	  */
-	  public EntityService() {
-		 // em = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME).createEntityManager();
-	  }
-	
-	 
-	   //EntityService as Singleton
-	    public static EntityService getInstance() {
+	private static final String PERSISTENCE_UNIT_NAME = "ch.bfh.btx8081.w2019.red.git";
+	protected static EntityManager em = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME)
+			.createEntityManager();
+	private static EntityService instance;
+
+	/**
+	 * 
+	 */
+	public EntityService() {
+		// em =
+		// Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME).createEntityManager();
+	}
+
+	/**
+	 * Creates EntityService Instance if there is no Instance.
+	 * @return EntryServise instance
+	 */
+	public static EntityService getInstance() {
 		if (instance == null) {
 			instance = new EntityService();
 			return instance;
 		}
-	return instance;
+		return instance;
 	}
-	
-	
-	
-	  /**
-	   * 
-	   */
-    public void setUp() {
-        // Begin a new local transaction so that we can persist a new entity
-        EntityTransaction transaction = em.getTransaction();
-        transaction.begin();
-        //em.flush();
-        //transaction.commit();
 
-	}
-	
-
-    public void close() {
-        // It is always good practice to close the EntityManager so that resources
-        // are conserved.
-        @SuppressWarnings("unused")
+	/**
+	 * Sets up Transaction
+	 */
+	public void setUp() {
+		// Begin a new local transaction so that we can persist a new entity
 		EntityTransaction transaction = em.getTransaction();
-        //transaction.begin();
-        // Query q = em.createNativeQuery("SHUTDOWN COMPACT");
-        // q.executeUpdate();
-        em.flush();
-        //transaction.commit();
-        em.close();
-    }
-	
+		transaction.begin();
+		// em.flush();
+		// transaction.commit();
+
+	}
+
+	/**
+	 * closes Transaction
+	 */
+	public void close() {
+		// It is always good practice to close the EntityManager so that resources
+		// are conserved.
+		@SuppressWarnings("unused")
+		EntityTransaction transaction = em.getTransaction();
+		// transaction.begin();
+		// Query q = em.createNativeQuery("SHUTDOWN COMPACT");
+		// q.executeUpdate();
+		em.flush();
+		// transaction.commit();
+		em.close();
+	}
+
+	/**
+	 * For Testing EntityService class
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		EntityService es = new EntityService();
-		//EntityManager em = es.getManager();
 		es.setUp();
 		es.close();
-		/*
-		ContactServiceClass csc = new ContactServiceClass();
-		Contact contact1 = new Contact();
-		City city1 = new City();
-		city1.setName("Bern");
-		city1.setZip(2555);
-		contact1.setCityId(city1.getId());
-		csc.addContact(contact1);
-		System.out.println(csc.addContact(contact1));
-		*/
-		//em.getTransaction();
-		//em.close();
+		
+
 	}
-	
+
 }
