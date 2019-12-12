96169ebdd06c601ac0454acd5d5a3a62882e3287
romap1 <56729339+romap1@users.noreply.github.com>
2019-12-12 23:03:21 +0100

    added a few tests

--- src/main/java/model/MainTestClass.java
+++ src/main/java/model/MainTestClass.java
@@ -4,26 +4,41 @@ public class MainTestClass {
 
 	public static void main(String[] args) {
 		
-		
+		// Test methods in ConactServiceClass 
 		
 		ContactServiceClass csc = new ContactServiceClass();
 		
-		//Contact contact1 = new Contact();
+		//Test addCity
 		City city1 = new City();
-		//city1.setId(12);
 		city1.setName("Bern");
 		city1.setZip(2555);
 		csc.addCity(city1);
+		//Test deleteCity. only works once
+		//csc.deleteCity(1); 
 		
-		/*
-		contact1.setCityId(city1.getId());
-		contact1.setFirstName("asd");
-		contact1.setMail("asdj");
-		contact1.setMobile("123");
+		//csc.deleteCity(city1.getId()); //doesn't work
+		
+		//getCity doesn't work
+		//System.out.println(csc.getCity(101));
+		
+		//Test addContact
+		Contact contact1 = new Contact();
+		
+		contact1.setCityId(51);
+		contact1.setFirstName("der");
+		contact1.setName("Ben");
+		contact1.setMail("asdj@bfh.ch");
+		contact1.setMobile("123 123 123");
 		contact1.setStatusId(Status.absent);
-		contact1.setStreet(street);
+		contact1.setTitle("DR");
+		//contact1.setUserId(13);
+		contact1.setStreet("Catstreet");
 		csc.addContact(contact1);
-		*/
+	
+		//setStreet() doesn't work
+		//
+		
+		
 		//System.out.println(csc.addContact(contact1));
 	}
 
