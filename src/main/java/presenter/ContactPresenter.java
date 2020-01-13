dac67381e956be70effb992a3a4fc9e8f118138a
gashf2 <56729393+gashf2@users.noreply.github.com>
2020-01-13 12:50:51 +0100

    Presenter for the contacts

--- src/main/java/presenter/ContactPresenter.java
+++ src/main/java/presenter/ContactPresenter.java
@@ -1,5 +1,48 @@
 package presenter;
 
-public class ContactPresenter {
+import java.util.List;
 
+import model.City;
+import model.Contact;
+import service.ContactService;
+
+/**
+ * Presenter for Contacts
+ * 
+ * @author gashf2
+ * 
+ */
+
+public class ContactPresenter {
+	private Contact contact;
+	private ContactService csc = new ContactService();
+
+	public void saveContact(String title, String firstName, String name, String mobile, String mail, String street) {
+		contact = new Contact();
+
+		contact.setTitle(title);
+		contact.setFirstName(firstName);
+		contact.setName(name);
+		contact.setMobile(mobile);
+		contact.setMail(mail);
+		contact.setStreet(street);
+
+		csc.addContact(contact);
+	}
+
+	public List<Contact> getAllContacts() {
+		return csc.getAllContacts();
+	}
+	
+	public List<City> getAllCities() {
+		return csc.getAllCities();
+	}
+
+	public Contact getContact(int id) {
+		return csc.getContact(id);
+	}
+
+	public void deleteContact(int id) {
+		csc.deleteContact(id);
+	}
 }
