fbad2ecf714c49267eebe3f9fa1a547d020030f7
gashf2 <56729393+gashf2@users.noreply.github.com>
2020-01-13 14:55:06 +0100

    added city to correct contact

--- src/main/java/presenter/ContactPresenter.java
+++ src/main/java/presenter/ContactPresenter.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import model.City;
 import model.Contact;
-import model.Status;
 import service.ContactService;
 
 /**
  * Presenter for Contacts
  * 
- * @author gashf2
+ * @author gashf2, regls1
  * 
  */
 
@@ -18,8 +17,17 @@ public class ContactPresenter {
 	private Contact contact;
 	private ContactService csc = new ContactService();
 
-	public void saveContact(String title, String firstName, String name, String mobile, String mail, String street, int kind) {
+	public void saveContact(String title, String firstName, String name, String mobile, String mail, String street,
+			int kind, String zip) {
+
 		contact = new Contact();
+		City city2 = null;
+		for(City city : csc.getAllCities()) {
+			if(city.getZip() == Integer.parseInt(zip)) {
+				city2 = csc.getCity(city.getId());
+			}
+			
+		}
 
 		contact.setTitle(title);
 		contact.setFirstName(firstName);
@@ -28,14 +36,24 @@ public class ContactPresenter {
 		contact.setMail(mail);
 		contact.setStreet(street);
 		contact.setUserId(kind);
+		contact.setCityId(city2.getId());
 
 		csc.addContact(contact);
+	}
+
+	public boolean checkCity(String zip) {
+		for (City selected : this.getAllCities()) {
+			if (selected.getZip() == Integer.parseInt(zip)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public List<Contact> getAllContacts() {
 		return csc.getAllContacts();
 	}
-	
+
 	public List<City> getAllCities() {
 		return csc.getAllCities();
 	}
@@ -47,8 +65,10 @@ public class ContactPresenter {
 	public void deleteContact(int id) {
 		csc.deleteContact(id);
 	}
-	
-	public void setStatus(int status) {
-		
+
+
+	public City getCity(int cityId) {
+		return csc.getCity(cityId);
 	}
+
 }
