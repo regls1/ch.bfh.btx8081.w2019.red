fce29dc1561c886ea2fa97f33c6cb04ee213f9b1
romap1 <56729339+romap1@users.noreply.github.com>
2020-01-14 14:37:10 +0100

    added comment

--- src/main/java/presenter/DiaryPresenter.java
+++ src/main/java/presenter/DiaryPresenter.java
@@ -6,17 +6,28 @@ import model.Entry;
 import service.DiaryService;
 import view.DiaryView;
 
+/**
+ * Presenter for Diary Entries.
+ * @author romap1
+ *
+ */
 public class DiaryPresenter {
 	
 	private DiaryService ds = new DiaryService();
 
 	
-	
+	/**
+	 * Returns List with all Entries from the database.
+	 * @return List with all Entries
+	 */
 	public List<Entry> getAllEntries() {
 		return ds.getAllEntries();
 		
 	}
-	
+	/**
+	 * Deletes a Entry.
+	 * @param entry, is a Diary entry 
+	 */
 	public void deleteEntry(Entry entry) {
 		ds.deleteEntry(entry.getId());
 		
