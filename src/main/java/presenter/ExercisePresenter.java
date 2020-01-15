ddbcee7033b587bbda7203e859434163a41cf4b6
ToWack <49730637+ToWack@users.noreply.github.com>
2020-01-15 01:09:22 +0100

    modified exercise presenter

--- src/main/java/presenter/ExercisePresenter.java
+++ src/main/java/presenter/ExercisePresenter.java
@@ -3,23 +3,81 @@ package presenter;
 import java.util.List;
 
 import model.Exercise;
+import model.Rating;
 import service.ExerciseService;
 
 /**
+ * exercise presenter
  * 
- * @author neues4
- *
+ * @author wackt2, neues4
  */
+
 public class ExercisePresenter {
-
-	// create new Exercise Service
-	private ExerciseService exService = new ExerciseService();
-
+	private Rating rating;
+	private ExerciseService service = new ExerciseService();
+	
 	/**
-	 * Method to return a list of all exercises from the database
-	 * @return List of all exercises form the database
+	 * method to return all existing exercises
+	 * @return List of all exercises
 	 */
 	public List<Exercise> getAllExercises() {
-		return exService.getAllExercises();
+		return service.getAllExercises();
 	}
+	
+	/**
+	 * method to return an existing exercise by id
+	 * @return exercise object
+	 */
+	public Exercise getExercise(int id) {
+		return service.getExercise(id);
+	}
+	
+	/**
+	 * method to return all existing ratings
+	 * @return List of all ratings
+	 */
+	public List<Rating> getAllRatings() {
+		return service.getAllRatings();
+	}
+	
+	/**
+	 * method to return an existing rating by id
+	 * @return contact object
+	 */
+	public Rating getRating(int id) {
+		return service.getRating(id);
+	}
+
+	/**
+	 * method to save a rating
+	 * 
+	 * @param value, rating value
+	 * @param userId, id of current user
+	 * @param exerciseId, id of exercise
+	 */
+	public void saveRating(int value, int userId, int exerciseId) {
+		// set all attributes
+		rating.setValue(value);
+		rating.setUserId(userId);
+		rating.setExerciseId(exerciseId);
+		
+		service.addRating(rating);
+	}
+	
+	/**
+	 * 
+	 * @param id, id of existing rating
+	 * @param value, rating value
+	 * @param userId, id of current user
+	 * @param exerciseId, id of exercise
+	 */
+	public void updateRating(int id, int value, int userId, int exerciseId) {
+		Rating r = this.getRating(id);
+		rating.setValue(value);
+		rating.setUserId(userId);
+		rating.setExerciseId(exerciseId);
+		
+		service.updateRating(r.getId(), rating);
+	}
+	
 }
