a50dcc38ee856d320317fc729699cd94cffad1b3
Selina Regli <56729195+regls1@users.noreply.github.com>
2020-01-16 11:27:34 +0100

    updated junit contact

--- src/main/java/test/ContactTest.java
+++ src/main/java/test/ContactTest.java
@@ -3,9 +3,9 @@ package test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
 import model.Contact;
 import presenter.ContactPresenter;
 import service.ContactService;
@@ -31,18 +31,17 @@ public class ContactTest {
 		return contactId;
 	}
 	
-	@Test
+	@Before
 	public void testAddContact() {
 		cPresenter.saveContact("Dr. med.", "Hansjürg", "Rubino", "0335554433", "hansjuerg.rubino@bfh.ch", "Bahnhofstrasse 33", 2, "3172");
 		assertTrue(cPresenter.checkCity("3172"));
-		//cPresenter.saveContact("Prof. Dr.", "Martin", "Müller", "0332211333", "martin.mueller@bfh.ch", "Dorfweg 102", 1, "8002");
-		//assertFalse(cPresenter.checkCity("8002"));
 	}
 	
 	@Test
 	public void testGetContact() {
 		int id = this.testGetContactId("hansjuerg.rubino@bfh.ch");
 		Contact contact = cPresenter.getContact(id);
+		System.out.println(contact.getId());
 		assertEquals(contact.getName(), "Rubino");
 	}
 	
@@ -53,22 +52,21 @@ public class ContactTest {
 		assertTrue(contact.getTitle().equals("Dr. med."));
 		assertFalse(contact.getTitle().equals("Prof. med."));
 		assertTrue(contact.getFirstName().equals("Hansjürg"));
-		assertFalse(contact.getFirstName().equals("Hansjürg"));
+		assertFalse(contact.getFirstName().equals("Hans Jürgs"));
 		assertTrue(contact.getName().equals("Rubino"));
-		assertFalse(contact.getName().equals("Rubino"));
+		assertFalse(contact.getName().equals("Rubin"));
 		assertTrue(contact.getMobile().equals("0335554433"));
-		assertFalse(contact.getMobile().equals("0335554433"));
+		assertFalse(contact.getMobile().equals("0315554433"));
 		assertTrue(contact.getStreet().equals("Bahnhofstrasse 33"));
-		assertFalse(contact.getStreet().equals("Bahnhofstrasse 33"));
+		assertFalse(contact.getStreet().equals("Bahnhofstrasse 2"));
 		assertTrue(contact.getUserId() == (2));
 		assertFalse(contact.getUserId() == (1));
 	}
 	
-	@Test
+	@After
 	public void testDeleteContact() {
 		int id = this.testGetContactId("hansjuerg.rubino@bfh.ch");
 		Contact contact = cPresenter.getContact(id);
 		cPresenter.deleteContact(id);
-		assertFalse(contact.getName().equals("Rubino"));
 	}
 }
